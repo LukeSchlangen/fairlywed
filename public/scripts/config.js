4e18c3b63bee9797f24fd5c5c841abd39132fe14
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-19 16:02:52 -0600

    initial commit

---
+++ public/scripts/config.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCvUVLVpjThEnm-HhxT00y3cWnr9nBxxAc",
+    authDomain: "farilywed.firebaseapp.com",
+    databaseURL: "https://farilywed.firebaseio.com",
+    storageBucket: "farilywed.appspot.com",
+    messagingSenderId: "802702759835"
+  };
+  firebase.initializeApp(config);
