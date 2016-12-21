49eaf3f2045afd29c192133d2e50c29d633d81d9
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-21 15:38:25 -0600

    changing use to appropriate get

--- server/routes/photographer-data.js
+++ server/routes/photographer-data.js
@@ -3,21 +3,7 @@ var router = express.Router();
 var pg = require('pg');
 var connectionString = require('../modules/database-config');
 
-// router.get("/", function(req, res){
-//   pg.connect(connectionString, function(err, client, done){
-//     client.query('SELECT * FROM subvendors JOIN subvendortypes ON subvendors.vendor_type = subvendortypes.id WHERE subvendortypes.name=$1 LIMIT 10', ['photographer'], function(err, photographerQueryResult){
-//       done();
-//       if(err){
-//         console.log('Error user data root GET SQL query task', err);
-//         res.sendStatus(500);
-//       }else{
-//         res.send({photographers: photographerQueryResult.rows});
-//       }
-//     });
-//   });
-// });
-
-router.use(function(req, res){
+router.get('/', function(req, res){
   pg.connect(connectionString, function(err, client, done){
     client.query('SELECT COALESCE(subvendors.name, vendors.name) AS name FROM subvendors JOIN subvendortypes ON subvendors.vendor_type = subvendortypes.id JOIN vendors ON vendors.id = subvendors.parent_vendor_id WHERE subvendortypes.name=$1 LIMIT 10;',
     ['photographer'],
