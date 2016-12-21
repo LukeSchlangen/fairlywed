bcad4f65a4b281abd014721acdfb62c7b7270f41
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-21 15:30:29 -0600

    photographer list data returning

---
+++ public/scripts/factories/photographer.factory.js
@@ -0,0 +1,24 @@
+app.factory("PhotographerFactory", ["$http",
+    function ($http) {
+        function getPhotographers() {
+            return $http({
+                method: 'GET',
+                url: '/photographerData',
+                headers: {
+                    private_data_requested: false
+                }
+            }).then(function (response) {
+                console.log('Photographer factory received data from the server: ', response.data);
+                return response.data;
+            }).catch(function(err){
+                console.error('Error retreiving photographer data: ', err);
+            });
+        }
+
+        return {
+            getPhotographers: function() {
+                return getPhotographers();
+            }
+        }
+    }
+]);
