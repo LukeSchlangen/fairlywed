36822fbf64b029acc360dff482c65731b59b11c7
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-25 08:27:25 -0600

    Adding packages to database

--- database.sql
+++ database.sql
@@ -24,9 +24,9 @@ CREATE TABLE vendors (
 );
 
 CREATE TABLE users_vendors (
-	id SERIAL PRIMARY KEY,
 	user_id INT NOT NULL REFERENCES users,
-	vendor_id INT NOT NULL REFERENCES vendors
+	vendor_id INT NOT NULL REFERENCES vendors,
+	PRIMARY KEY(user_id, vendor_id)
 );
 
 CREATE TABLE subvendortypes (
@@ -40,14 +40,35 @@ CREATE TABLE subvendors (
 	location geography, -- if null, pull value from the parent
 	travelDistance INT, -- if null, pull value from the parent
 	parent_vendor_id INT NOT NULL REFERENCES vendors,
-	vendor_type INT NOT NULL REFERENCES subvendortypes,
+	vendortype_id INT NOT NULL REFERENCES subvendortypes,
 	url_slug VARCHAR(200) UNIQUE NOT NULL,
 	is_active BOOLEAN DEFAULT TRUE NOT NULL
+);
+
+CREATE TABLE packages(
+	id SERIAL PRIMARY KEY,
+	vendortype_id INT NOT NULL REFERENCES subvendortypes,
+	name VARCHAR(500) UNIQUE NOT NULL,
+	is_active BOOLEAN DEFAULT TRUE NOT NULL
+);
+
+CREATE TABLE subvendors_packages (
+	subvendor_id INT NOT NULL REFERENCES subvendors,
+	package_id INT NOT NULL REFERENCES packages,
+	price INT CHECK (price > 100 AND price <100000),
+	is_active BOOLEAN DEFAULT TRUE NOT NULL,
+	PRIMARY KEY(subvendor_id, package_id)
 );
 
 CREATE TABLE clients (
 	id SERIAL PRIMARY KEY,
 	user_id INT NOT NULL REFERENCES users
+);
+
+CREATE TABLE users_clients (
+	user_id INT NOT NULL REFERENCES users,
+	client_id INT NOT NULL REFERENCES clients,
+	PRIMARY KEY(user_id, client_id)
 );
 
 
@@ -64,7 +85,7 @@ VALUES ('Big Time Minnetonka Wedding Vendor', CAST(ST_SetSRID(ST_Point(-93.4687,
 ('The Bloomington Wedding Vendor', CAST(ST_SetSRID(ST_Point(-86.5264, 39.1653),4326) As geography));
     
 -- INSERTING SUBVENDORS
-INSERT INTO subvendors (name, parent_vendor_id, vendor_type, url_slug, location)
+INSERT INTO subvendors (name, parent_vendor_id, vendortype_id, url_slug, location)
 VALUES ('Minnetonka Photography', 1, 1, 'minnetonka-photography', null),
 ('Minnetonka Videography', 1, 2, 'minnetonka-videography', null),
 ('Minnetonka DJ', 1, 3, 'minnetonka-dj', CAST(ST_SetSRID(ST_Point(-93.3687, 45.0212),4326) As geography)), -- the dj is stationed out of a different office and has a different location
@@ -72,9 +93,9 @@ VALUES ('Minnetonka Photography', 1, 1, 'minnetonka-photography', null),
 ('Bloomington Wedding Photography', 3, 1, 'bloomington-wedding-photography', null),
 (null, 3, 2, 'bloomington-videography', null);
 
+
 -- SAMPLE QUERIES
 
--- VENDORS SERVING GIVEN LOCATION
 -- Vendors who service Eden Prairie, lat and long need to be passed in and the results are in meters
 -- The Latitude and longitude of the client's current location need to be passed in to this query
 SELECT COALESCE(subvendors.name, vendors.name), ST_Distance((SELECT COALESCE(subvendors.location, vendors.location)), CAST(ST_SetSRID(ST_Point(-93.4708, 44.8547),4326) As geography))
@@ -84,3 +105,7 @@ WHERE (SELECT ST_Distance(
 		(SELECT COALESCE(subvendors.location, vendors.location)),
 		(CAST(ST_SetSRID(ST_Point(-93.4708, 44.8547),4326) As geography))
 	)) < (SELECT COALESCE(subvendors.travelDistance, vendors.travelDistance)); -- This query would also need additional AND statements to limit to specific types of vendors, like photographers
+
+-- Only Returning photographers (regardless of location)
+SELECT COALESCE(subvendors.name, vendors.name) FROM subvendors JOIN subvendortypes ON subvendors.vendortype_id = subvendortypes.id 
+JOIN vendors ON vendors.id = subvendors.parent_vendor_id WHERE subvendortypes.name='photographer' LIMIT 10;
--- server/routes/photographer-data.js
+++ server/routes/photographer-data.js
@@ -5,7 +5,7 @@ var connectionString = require('../modules/database-config');
 
 router.get('/', function(req, res){
   pg.connect(connectionString, function(err, client, done){
-    client.query('SELECT COALESCE(subvendors.name, vendors.name) AS name FROM subvendors JOIN subvendortypes ON subvendors.vendor_type = subvendortypes.id JOIN vendors ON vendors.id = subvendors.parent_vendor_id WHERE subvendortypes.name=$1 LIMIT 10;',
+    client.query('SELECT COALESCE(subvendors.name, vendors.name) AS name FROM subvendors JOIN subvendortypes ON subvendors.vendortype_id = subvendortypes.id JOIN vendors ON vendors.id = subvendors.parent_vendor_id WHERE subvendortypes.name=$1 LIMIT 10;',
     ['photographer'],
     function(err, photographerQueryResult){
       done();
