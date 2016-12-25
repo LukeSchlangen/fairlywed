3c9e2e1e266f26b7f2c500365c62c3dc7fed2234
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-25 13:47:56 -0600

    indenting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,14 +14,14 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             templateUrl: 'views/photographers.html'
         })
         .state('home.photographers.search', {
-        url: '/search?package&param1&param2',
-        template: '<h1>new!!!!!!!!!!</h1>',
-        controller: function($scope, $stateParams) {
-            $scope.package = $stateParams.package;
-            $scope.param1 = $stateParams.param1;
-            $scope.param2 = $stateParams.param2;
-            console.log('$stateParams: ', $stateParams);
-        }
+            url: '/search?package&param1&param2',
+            template: '<h1>new!!!!!!!!!!</h1>',
+            controller: function ($scope, $stateParams) {
+                $scope.package = $stateParams.package;
+                $scope.param1 = $stateParams.param1;
+                $scope.param2 = $stateParams.param2;
+                console.log('$stateParams: ', $stateParams);
+            }
         })
         .state('home.videographers', {
             url: '/videographers',
