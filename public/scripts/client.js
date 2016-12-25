dee6ee9e93e99d6b32b4f19cfa6d1e1ba4ed0319
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-25 12:02:54 -0600

    beginning of querying

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,6 +13,22 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             url: '/photographers',
             templateUrl: 'views/photographers.html'
         })
+.state('home.photographers.new-qs', {
+  url: 'home/new?portfolioId&param1&param2',
+  template: '<h1>new!!!!!!!!!!</h1>',
+  controller: function($scope, $stateParams) {
+     $scope.portfolioId = $stateParams.portfolioId;
+     $scope.param1 = $stateParams.param1;
+     $scope.param2 = $stateParams.param2;
+  }
+})
+//         state('new-qs', {
+//   url: '/new?portfolioId',
+//   templateUrl: 'new.html',
+//   controller: function($scope, $stateParams) {
+//      $scope.portfolioId = $stateParams.portfolioId;
+//   }
+// })
         .state('home.videographers', {
             url: '/videographers',
             templateUrl: 'views/videographers.html'
