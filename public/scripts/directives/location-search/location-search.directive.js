9397a892e839494b969a1a1775500654692d11c0
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-28 22:14:20 -0600

    location search working

--- public/scripts/directives/location-search/location-search.directive.js
+++
@@ -1,9 +0,0 @@
-app.directive('locationSearch', function () {
-    return {
-        restrict: 'E',
-        scope: {},
-        templateUrl: 'scripts/directives/location-search/location-search.directive.html',
-        controller: 'LocationSearchController',
-        controllerAs: 'vm'
-    };
-});
--- public/scripts/directives/photographer-search/photographer-search.controller.js
+++ public/scripts/directives/photographer-search/photographer-search.controller.js
@@ -1,10 +1,21 @@
-app.controller("PhotographerSearchController", ["PhotographerFactory",
-    function (PhotographerFactory) {
+app.controller("PhotographerSearchController", ["PhotographerFactory", "$scope", 
+    function (PhotographerFactory, $scope) {
         var self = this;
-        self.search={};
+        self.search = {package: "Two Photographers: 8 Hours"};
         self.packages = PhotographerFactory.packages;
-        self.updatePhotographersList = function() {
+        self.updatePhotographersList = function () {
             PhotographerFactory.updatePhotographersList(self.search);
         };
+        self.initialize = function() {
+            var input = document.getElementById('searchTextField');
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            console.log("Initializing google maps");
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var place = autocomplete.getPlace();
+                self.search.latitude = place.geometry.location.lat();
+                self.search.longitude = place.geometry.location.lng();
+                self.updatePhotographersList();
+            });
+        }
     }
 ]);
