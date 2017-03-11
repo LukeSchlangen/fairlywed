4c589781148152aae57c35a3f8019e61624a0091
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-11 12:05:49 -0600

    Cleaning subvendor-details

--- public/components/subvendor-details/subvendor-details.controller.js
+++ public/components/subvendor-details/subvendor-details.controller.js
@@ -11,59 +11,59 @@ app.controller("SubvendorDetailsController", ["$http", "AuthFactory", "$statePar
         getAllPackages();
 
         function getAllPackages() {
-        AuthFactory.$onAuthStateChanged(function (firebaseUser) {
-            // firebaseUser will be null if not logged in
-            console.log('AuthFactory check inside of VendorDetails Controller Triggered');
-            if (firebaseUser) {
-                // This is where we make our call to our server
-                firebaseUser.getToken().then(function (idToken) {
-                    $http({
-                        method: 'GET',
-                        url: '/subvendorDetailsData',
-                        headers: {
-                            id_token: idToken,
-                            subvendor_id: $stateParams.subvendorId
-                        }
-                    }).then(function (response) {
-                        console.log('subvendor details controller returned: ', response.data);
-                        self.packages.list = response.data.packages;
-                    }).catch(function (err) {
-                        console.error('Error retreiving private user data: ', err);
+            AuthFactory.$onAuthStateChanged(function (firebaseUser) {
+                // firebaseUser will be null if not logged in
+                console.log('AuthFactory check inside of VendorDetails Controller Triggered');
+                if (firebaseUser) {
+                    // This is where we make our call to our server
+                    firebaseUser.getToken().then(function (idToken) {
+                        $http({
+                            method: 'GET',
+                            url: '/subvendorDetailsData',
+                            headers: {
+                                id_token: idToken,
+                                subvendor_id: $stateParams.subvendorId
+                            }
+                        }).then(function (response) {
+                            console.log('subvendor details controller returned: ', response.data);
+                            self.packages.list = response.data.packages;
+                        }).catch(function (err) {
+                            console.error('Error retreiving private user data: ', err);
+                        });
                     });
-                });
-            } else {
-                console.log('Not logged in or not authorized.');
-            }
-        });
+                } else {
+                    console.log('Not logged in or not authorized.');
+                }
+            });
         }
 
-        self.savePackage = function(packageToSave) {
+        self.savePackage = function (packageToSave) {
             console.log(packageToSave);
             AuthFactory.$onAuthStateChanged(function (firebaseUser) {
-            // firebaseUser will be null if not logged in
-            console.log('AuthFactory check inside of VendorDetails Controller Triggered');
-            if (firebaseUser) {
-                // This is where we make our call to our server
-                firebaseUser.getToken().then(function (idToken) {
-                    $http({
-                        method: 'POST',
-                        url: '/subvendorDetailsData',
-                        headers: {
-                            id_token: idToken,
-                            subvendor_id: $stateParams.subvendorId
-                        },
-                        data: packageToSave
-                    }).then(function (response) {
-                        console.log('subvendor details controller returned: ', response.data);
-                        getAllPackages();
-                    }).catch(function (err) {
-                        console.error('Error retreiving private user data: ', err);
+                // firebaseUser will be null if not logged in
+                console.log('AuthFactory check inside of VendorDetails Controller Triggered');
+                if (firebaseUser) {
+                    // This is where we make our call to our server
+                    firebaseUser.getToken().then(function (idToken) {
+                        $http({
+                            method: 'POST',
+                            url: '/subvendorDetailsData',
+                            headers: {
+                                id_token: idToken,
+                                subvendor_id: $stateParams.subvendorId
+                            },
+                            data: packageToSave
+                        }).then(function (response) {
+                            console.log('subvendor details controller returned: ', response.data);
+                            getAllPackages();
+                        }).catch(function (err) {
+                            console.error('Error retreiving private user data: ', err);
+                        });
                     });
-                });
-            } else {
-                console.log('Not logged in or not authorized.');
-            }
-        });
+                } else {
+                    console.log('Not logged in or not authorized.');
+                }
+            });
         }
     }
 ]);
