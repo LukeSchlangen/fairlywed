b9b6430d58100fc4629da45cf78dec61985f6194
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-27 20:40:49 -0500

    Cleaning front end environment variable creation

--- gulpfile.js
+++ gulpfile.js
@@ -9,39 +9,42 @@ var concat = require('gulp-concat');
 
 // for environment variables
 var m = {};
- 
-gulp.task('scripts', function() {
-  return gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/'));
+
+gulp.task('scripts', function () {
+    return gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('dotenvToJson', () => {
     copy({
-        keys : ['FIREBASE_API_KEY', 
-                'FIREBASE_AUTH_DOMAIN', 
-                'FIREBASE_DATABASE_URL', 
-                'FIREBASE_STORAGE_BUCKET', 
-                'FIREBASE_MESSAGING_SENDER_ID' ],
-        paths : {
-            env : '.env',
-            jenv : 'env.json'
+        keys: ['FIREBASE_API_KEY',
+            'FIREBASE_AUTH_DOMAIN',
+            'FIREBASE_DATABASE_URL',
+            'FIREBASE_STORAGE_BUCKET',
+            'FIREBASE_MESSAGING_SENDER_ID'],
+        paths: {
+            env: '.env',
+            jenv: 'env.json'
         }
     });
-    gulp.src(['./env-build/part1.js','./env.json', './env-build/part2.js'])
-    .pipe(concat('firebase.config.js', {newLine: ''}))
-    .pipe(gulp.dest('dist/public/scripts'));
-    // gulp.src('.env')
-    // .pipe(gulp.dest('dist'));
-    // fs.writeFileSync('dist/public/scripts/firebase.config.js', 'var firebaseConfig = ' + fs.readFile('.env') + '; firebase.initializeApp(firebaseConfig);');
-
 });
 
+gulp.task('moveHTML', () => {
+    gulp.src(['public/**/**', 'node_modules/**/*.*'], { base: '.' })
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['moveHTML', 'dotenvToJson']);
+
+
+
+// ----------- START CODE TO CREATE FIREBASE CONFIG VARIABLES FROM ENVIRONMENT VARIABLES -------------- //
 function validateParams(params) {
     var attrs = ['keys', 'paths'];
     for (var i in attrs) {
         if (!params[attrs[i]]) {
-            throw attrs[i]+' must be truthy';
+            throw attrs[i] + ' must be truthy';
         }
     }
 
@@ -72,22 +75,22 @@ function copy(params) {
         }
     } else {
         // If .env file does not exist, create .env.json from environment variables
-        keys.forEach(function(key) {
+        keys.forEach(function (key) {
             o[key] = process.env[key];
         });
     }
-    fs.writeFile(paths.jenv, JSON.stringify(o));
-    console.log('----- json env version written successfully -------');
+
+
+    var firebaseConfigText = 'var config =  ' + JSON.stringify(o) + ';' +
+        '  const firebaseConfig = {' +
+        '    apiKey: config.FIREBASE_API_KEY,' +
+        '    authDomain: config.FIREBASE_AUTH_DOMAIN,' +
+        '    databaseURL: config.FIREBASE_DATABASE_URL,' +
+        '    storageBucket: config.FIREBASE_STORAGE_BUCKET,' +
+        '    messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID' +
+        '  };' +
+        'firebase.initializeApp(firebaseConfig);'
+
+    fs.writeFile('dist/public/scripts/firebase.config.js', firebaseConfigText);
 };
-
-
-module.exports = m;
-
- gulp.task('moveHTML', () => {
-     gulp.src(['public/**/**', 'node_modules/**/*.*'], {base: '.'})
-     .pipe(gulp.dest('dist'))
- });
-
-gulp.task('default', ['moveHTML','dotenvToJson']);
-
-
+// ----------- END CODE TO CREATE FIREBASE CONFIG VARIABLES FROM ENVIRONMENT VARIABLES -------------- //
