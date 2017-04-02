75ff5de648220e30f9ab8576842f7efef0c7e2fd
Luke Schlangen <lukeschlangen@gmail.com>
2017-04-02 12:56:49 -0500

    Hard-coded, unauthorized image retrieval working

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,15 +1,5 @@
-// upload.js
-
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-// var multer = require('multer');
-// var upload = multer({dest: 'uploads/'});
-var pool = require('../modules/pg-pool');
-
-// From gCloud demo
-// var admin = require('../modules/initialized-firebase-admin');
-const format = require('util').format;
 const Multer = require('multer');
 const multer = Multer({
   storage: Multer.memoryStorage(),
@@ -18,17 +8,11 @@ const multer = Multer({
   }
 });
 
-// By default, the client will authenticate using the service account file
-// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
-// the project specified by the GCLOUD_PROJECT environment variable. See
-// https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
-// These environment variables are set automatically on Google App Engine
 const Storage = require('@google-cloud/storage');
 
-// Instantiate a storage client
 const storage = Storage({
-    keyFilename: 'server/firebase-service-account.json',
-    projectId: process.env.FIREBASE_PROJECT_ID
+  keyFilename: 'server/firebase-service-account.json',
+  projectId: process.env.FIREBASE_PROJECT_ID
 });
 
 const bucket = storage.bucket(process.env.FIREBASE_STORAGE_BUCKET);
@@ -52,64 +36,28 @@ router.post('/', multer.single('file'), (req, res, next) => {
   });
 
   blobStream.on('finish', () => {
-    // The public URL can be used to directly access the file via HTTP.
-    const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
-    res.status(200).send(publicUrl);
+    res.status(200).send('The image has been successfully uploaded to google cloud storage');
   });
 
   blobStream.end(req.file.buffer);
 });
 
+router.get('/:imageId', function (req, res, next) {
+  var stream = bucket.file('Toast.jpg').createReadStream();
+
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+
+  stream.on('data', function (data) {
+    res.write(data);
+  });
+
+  stream.on('error', function (err) {
+    console.log('error reading stream', err);
+  });
+
+  stream.on('end', function () {
+    res.end();
+  });
+});
+
 module.exports = router;
-
-
-
-
-// post to database
-  // pool.connect(function(err, client, done){
-  //   var userId = req.decodedToken.userSQLId;
-  //   client.query('INSERT INTO users VALUES () FROM users WHERE id=$1', [userId], function(err, userDataQueryResult){
-  //     done();
-  //     if(err){
-  //       console.log('Error user data root GET SQL query task', err);
-  //       res.sendStatus(500);
-  //     }else{
-  //       res.send({name: userDataQueryResult.rows[0].name});
-  //     }
-  //   });
-  // });
-
-
-
-// /**
-//  * Gets the list of all files from the database
-//  */
-// router.get('/', function (req, res, next) {
-//   Upload.find({},  function (err, uploads) {
-//     if (err) next(err);
-//     else {
-//       res.send(uploads);
-//     }
-//   });
-// });
-
-// /**
-//  * Gets a file from the hard drive based on the unique ID and the filename
-//  */
-// router.get('/:uuid/:filename', function (req, res, next) {
-//   console.log(req.params);
-//   Upload.findOne({
-//     'file.filename': req.params.uuid,
-//     'file.originalname': req.params.filename
-//   }, function (err, upload) {
-
-//     if (err) next(err);
-//     else {
-//       res.set({
-//         "Content-Disposition": 'attachment; filename="' + upload.file.originalname + '"',
-//         "Content-Type": upload.file.mimetype
-//       });
-//       fs.createReadStream(upload.file.path).pipe(res);
-//     }
-//   });
-// });
