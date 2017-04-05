9b9218f7f872b4ddd8c8bcb53d5e4bb4e53e233e
Miles <MilesD92@msn.com>
2017-04-04 19:35:33 -0500

    fixed merge conflicts

--- public/components/photographer-profile/photographer-profile-booking/photographer-profile-booking.controller.js
+++ public/components/photographer-profile/photographer-profile-booking/photographer-profile-booking.controller.js
@@ -1,9 +1,18 @@
-app.controller("PhotographerProfileBookingController", ["PhotographerSearchFactory",
-    function (PhotographerSearchFactory) {
+app.controller("PhotographerProfileBookingController", ["PhotographerSearchFactory", "AuthFactory",
+    function (PhotographerSearchFactory, AuthFactory) {
         var self = this;
+        self.isLoggedIn = false;
+        if (!AuthFactory.$getAuth()) {
+            AuthFactory.$signInWithPopup('google').then(() => {
+                self.isLoggedIn = true;
+            });
+        } else {
+            self.isLoggedIn = true;
+        }
 
         PhotographerSearchFactory.updateSubvendorProfileDetails();
 
         self.currentSubvendor = PhotographerSearchFactory.currentSubvendor;
     }
+
 ]);
