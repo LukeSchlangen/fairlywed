8a0e1121442155fc3f44a7288b569e71a1d03d1e
Luke Schlangen <lukeschlangen@gmail.com>
2017-04-08 14:52:10 -0500

    Adding back angular material

--- public/components/photographer-profile/photographer-profile-gallery/photographer-profile-gallery.controller.js
+++ public/components/photographer-profile/photographer-profile-gallery/photographer-profile-gallery.controller.js
@@ -1,5 +1,5 @@
 app.controller("PhotographerProfileGalleryController", function (PublicImagesFactory) {
     var self = this;
-
+    PublicImagesFactory.updateImagesList();
     self.images = PublicImagesFactory.images;
 });
