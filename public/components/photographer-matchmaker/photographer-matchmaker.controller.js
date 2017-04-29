772546f2eb017e90983fe66455c64793961df54f
Miles <MilesD92@msn.com>
2017-04-29 16:38:56 -0500

    refactor based on feedback in PR

--- public/components/photographer-matchmaker/photographer-matchmaker.controller.js
+++ public/components/photographer-matchmaker/photographer-matchmaker.controller.js
@@ -1,11 +1,8 @@
 app.controller("PhotographerMatchmakerController", ["PhotographerSearchFactory", "PhotographerMatchmakerFactory", "AuthFactory",
     function (PhotographerSearchFactory, PhotographerMatchmakerFactory, AuthFactory) {
         var self = this;
-        if (AuthFactory.currentUser) {
-            PhotographerMatchmakerFactory.getPhotos();
-        } else {
-            AuthFactory.$onAuthStateChanged(PhotographerMatchmakerFactory.getPhotos);
-        }
+
+        AuthFactory.$onAuthStateChanged(PhotographerMatchmakerFactory.getPhotos);
 
         self.photos = PhotographerMatchmakerFactory.photos;
         self.clickPhotos = function (photoId) {
