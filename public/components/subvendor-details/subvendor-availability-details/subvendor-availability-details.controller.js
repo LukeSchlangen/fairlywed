63e29eb8505ac56afa62064fb8f217b873704d17
Luke Schlangen <lukeschlangen@gmail.com>
2017-04-29 20:34:40 -0500

    Calendar working and triggering on all day and month clicks

--- public/components/subvendor-details/subvendor-availability-details/subvendor-availability-details.controller.js
+++ public/components/subvendor-details/subvendor-availability-details/subvendor-availability-details.controller.js
@@ -1,4 +1,4 @@
-app.controller("SubvendorAvailabilityDetailsController", function (SubvendorFactory) {
+app.controller("SubvendorAvailabilityDetailsController", function (SubvendorFactory, $filter) {
     var self = this;
 
     self.subvendor = SubvendorFactory.subvendor;
@@ -20,4 +20,57 @@ app.controller("SubvendorAvailabilityDetailsController", function (SubvendorFact
         }
         SubvendorFactory.updateAvailability(availability);
     }
+
+
+
+
+
+    self.msg = {};
+    self.msg.text = "This is a message!"
+
+    self.dayFormat = "d";
+
+    // To select a single date, make sure the ngModel is not an array.
+    self.selectedDate = null;
+
+    // If you want multi-date select, initialize it as an array.
+    self.selectedDate = '';
+
+    self.firstDayOfWeek = 0; // First day of the week, 0 for Sunday, 1 for Monday, etc.
+    self.setDirection = function (direction) {
+        self.direction = direction;
+        self.dayFormat = direction === "vertical" ? "EEEE, MMMM d" : "d";
+    };
+
+    self.dayClick = function (date) {
+        self.msg.text = "You clicked " + $filter("date")(date, "MMM d, y h:mm:ss a Z");
+    };
+
+    self.prevMonth = function (data) {
+        self.msg.text = "You clicked (prev) month " + data.month + ", " + data.year;
+    };
+
+    self.nextMonth = function (data) {
+        self.msg.text = "You clicked (next) month " + data.month + ", " + data.year;
+    };
+
+    self.tooltips = true;
+    self.setDayContent = function (date) {
+
+        // You would inject any HTML you wanted for
+        // that particular date here.
+        console.log(date);
+        return self.msg.text;
+
+        //     // You could also use an $http function directly.
+        //     return $http.get("/some/external/api");
+
+        //     // You could also use a promise.
+        //     var deferred = $q.defer();
+        //     $timeout(function() {
+        //         deferred.resolve("<p></p>");
+        //     }, 1000);
+        //     return deferred.promise;
+
+    };
 });
