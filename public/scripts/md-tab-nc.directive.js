876ed13e80ae6f6b762ec20a6a43d83de996cfaa
Luke Schlangen <lukeschlangen@gmail.com>
2017-04-30 19:23:40 -0500

    Allow returning to child state views

---
+++ public/scripts/md-tab-nc.directive.js
@@ -0,0 +1,19 @@
+app.directive('mdActiveNc', MdActiveNoClick);
+
+function MdActiveNoClick() {
+    return {
+        link: link,
+        require: '^?mdTabs',
+        restrict: 'A'
+    };
+
+    function link(scope, element, attributes, ctrl) {
+        var index = ctrl.getTabElementIndex(element);
+        scope.$watch(attributes.mdActiveNc, function (active) {
+            if (active) {
+                ctrl.focusIndex = ctrl.selectedIndex = index;
+                ctrl.lastClick = true;
+            }
+        });
+    }
+}
