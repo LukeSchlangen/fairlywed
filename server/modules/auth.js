0280adb306d29ba5b4b7bb2dac8728133f6ec017
Luke Schlangen <lukeschlangen@gmail.com>
2017-05-05 22:04:06 -0500

    Simplify anonymous user becoming nonymous

--- server/modules/auth.js
+++ server/modules/auth.js
@@ -66,68 +66,18 @@ var linkPreviouslyAnonymousUser = function (req, res, next) {
     admin.auth().verifyIdToken(req.headers.previously_anonymous_id_token).then(function (previousAnonymousUserDecodedToken) {
       pool.connect(function (err, client, done) {
         var previousAnonymousFirebaseUserId = previousAnonymousUserDecodedToken.user_id || previousAnonymousUserDecodedToken.uid;
-        client.query('SELECT id FROM users WHERE firebase_user_id=$1', [previousAnonymousFirebaseUserId], function (err, previousAnonymousUserSQLIdResult) {
-          done();
-          if (err) {
-            console.log('Error completing user id query task', err);
-            res.sendStatus(500);
-          } else {
-            if (previousAnonymousUserSQLIdResult.rows.length === 0) {
-              // If the anonymous user is not in the database, move on
-              next();
+        client.query(`WITH log_update AS (UPDATE logs SET user_id=$1 WHERE user_id=(SELECT id FROM users WHERE firebase_user_id=$2) RETURNING id), 
+          matchmaker_run_update AS (UPDATE matchmaker_run SET user_id=$1 WHERE user_id=(SELECT id FROM users WHERE firebase_user_id=$2) RETURNING id)
+          DELETE FROM users WHERE id=(SELECT id FROM users WHERE firebase_user_id=$2)`,
+          [req.decodedToken.userSQLId, previousAnonymousFirebaseUserId], function (err) {
+            done();
+            if (err) {
+              console.log('Error updating previousAnonymousFirebaseUser in database', err);
+              res.sendStatus(500);
             } else {
-              var previousAnonymousFirebaseUserId = previousAnonymousUserSQLIdResult.rows[0].id;
-              // Change all of the references to the anonymous user to the non-anonymous user
-              // TODO: SOME UPDATE QUERY to Change everything in the ranking table that Miles is creating
-              pool.connect(function (err, client, done) {
-                if (err) {
-                  console.log('Error connecting to database', err);
-                  res.sendStatus(500);
-                } else {
-                  client.query('UPDATE logs SET user_id=$1 WHERE user_id=$2', [req.decodedToken.userSQLId, previousAnonymousFirebaseUserId], function (err) {
-                    done();
-                    if (err) {
-                      console.log('Error updating logs in database', err);
-                      res.sendStatus(500);
-                    } else {
-                      pool.connect(function (err, client, done) {
-                        if (err) {
-                          console.log('Error connecting to database', err);
-                          res.sendStatus(500);
-                        } else {
-                          client.query('UPDATE matchmaker_run SET user_id=$1 WHERE user_id=$2', [req.decodedToken.userSQLId, previousAnonymousFirebaseUserId], function (err) {
-                            done();
-                            if (err) {
-                              console.log('Error updating matchmaker_run in database', err);
-                              res.sendStatus(500);
-                            } else {
-                              pool.connect(function (err, client, done) {
-                                if (err) {
-                                  console.log('Error connecting to database', err);
-                                  res.sendStatus(500);
-                                } else {
-                                  client.query('DELETE FROM users WHERE id=$1', [previousAnonymousFirebaseUserId], function (err) {
-                                    done();
-                                    if (err) {
-                                      console.log('Error updating removing anonymous user from database', err);
-                                      res.sendStatus(500);
-                                    } else {
-                                      next();
-                                    }
-                                  });
-                                }
-                              });
-                            }
-                          });
-                        }
-                      });
-                    }
-                  });
-                }
-              });
+              next();
             }
-          }
-        });
+          });
       });
     });
   } else {
