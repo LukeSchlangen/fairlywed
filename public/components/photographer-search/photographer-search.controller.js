c9094cfa9e9157a888ca3cb26100e6c0d24ae0bb
Luke Schlangen <lukeschlangen@gmail.com>
2017-07-23 10:10:01 -0500

    Single line package component search bar

--- public/components/photographer-search/photographer-search.controller.js
+++ public/components/photographer-search/photographer-search.controller.js
@@ -1,6 +1,7 @@
-app.controller("PhotographerSearchController", function (PhotographerSearchFactory, PackagesFactory, $scope) {
+app.controller("PhotographerSearchController", function (PhotographerSearchFactory, PackagesFactory) {
     var self = this;
     self.search = PhotographerSearchFactory.search;
+    self.packageComponents = PhotographerSearchFactory.packageComponents;
     self.packages = PackagesFactory.packages;
     self.updatePhotographersList = PhotographerSearchFactory.updatePhotographersList;
     self.updatePhotographersList(); // adds the parameters back to url on return to view
