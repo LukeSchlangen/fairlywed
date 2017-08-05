5c775e150197dde6281d9fa247d6ecb2750946ef
Luke Schlangen <lukeschlangen@gmail.com>
2017-08-05 09:41:50 -0500

    Adding https rediect logs

--- server/modules/enforce-ssl.js
+++ server/modules/enforce-ssl.js
@@ -3,7 +3,11 @@ var env = process.env.NODE_ENV || 'development';
 var forceSSL = function (req, res, next) {
     if (env === 'production') {
         if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(['https://', req.get('Host'), req.url].join(''));
+            var urlBeforeConversion = req.url;
+            var urlAfterConversion = ['https://', req.get('Host'), req.url].join('');
+            console.log('urlBeforeConversion: ', urlBeforeConversion);
+            console.log('urlAfterConversion: ', urlAfterConversion);
+            return res.redirect(urlAfterConversion);
         }
     }
     return next();
