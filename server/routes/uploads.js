677c19604ce13ff49c489daa3dcf1e62442d452d
Luke Schlangen <lukeschlangen@gmail.com>
2017-08-24 21:37:59 -0500

    Resizing images and restricting file types

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,29 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pg-pool');
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminJpegoptim = require('imagemin-jpegoptim');
-const imageminPngquant = require('imagemin-pngquant');
+var sharp = require('sharp');
+var imagemin = require('imagemin');
+var imageminJpegoptim = require('imagemin-jpegoptim');
 var bucket = require('../modules/google-storage-bucket');
 var Multer = require('multer');
 var multer = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+    fileSize: 50 * 1024 * 1024 // no larger than 50mb, you can change as needed.
   }
 });
 
 // Process the file upload and upload to Google Cloud Storage.
 router.post('/', multer.single('file'), (req, res) => {
-  if (!req.file) {
-    res.status(400).send('No file uploaded.');
-    return;
-  }
-
   var file = req.file;
   var userId = req.decodedToken.userSQLId;
   var subvendorId = req.headers.subvendor_id;
+
+  if (!file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  } 
+  
+  var isAcceptedImageType = file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/gif';
+  
+  if (!isAcceptedImageType) {
+    res.status(400).send('Unrecognized image type. Must by jpeg, png, or gif');
+    return;
+  }
 
   pool.connect(function (err, client, done) {
     client.query('INSERT INTO subvendor_images (original_name, encoding, mime_type, subvendor_id) '
@@ -39,36 +45,48 @@ router.post('/', multer.single('file'), (req, res) => {
           console.log('Error image INSERT SQL query task', err);
           res.sendStatus(500);
         } else {
-          var newImageId = imageId.rows[0].id.toString();
-          // Create a new blob in the bucket and upload the file data.
-          var blob = bucket.file(newImageId);
-          var blobStream = blob.createWriteStream();
+          sharp(file.buffer)
+            .resize(1800, 1800)
+            .max()
+            .withoutEnlargement()
+            .toFormat('jpeg')
+            .toBuffer()
+            .then(function (reducedSizeImageBuffer) {
+              imagemin.buffer(reducedSizeImageBuffer, {
+                plugins: [
+                  imageminJpegoptim({ max: 80 })
+                ]
+              }).then(function (compressedImageBuffer) {
+                var newImageId = imageId.rows[0].id.toString();
+                // Create a new blob in the bucket and upload the file data.
+                var blob = bucket.file(newImageId);
+                var blobStream = blob.createWriteStream();
 
-          blobStream.on('error', (err) => {
-            console.log('error', err);
-            res.send(500);
-          });
+                blobStream.on('error', (err) => {
+                  console.log('error', err);
+                  res.sendStatus(500);
+                });
 
-          blobStream.on('finish', () => {
-            client.query('UPDATE subvendor_images SET is_active=TRUE WHERE id = $1', [newImageId], function (err) {
-              if (err) {
-                console.log('error updating is_active status of new image:', err);
+                blobStream.on('finish', () => {
+                  client.query('UPDATE subvendor_images SET is_active=TRUE WHERE id = $1', [newImageId], function (err) {
+                    if (err) {
+                      console.log('error updating is_active status of new image:', err);
+                      res.sendStatus(500);
+                    } else {
+                      res.end();
+                    }
+                  });
+                });
+
+                blobStream.end(compressedImageBuffer);
+              }).catch(function (err) {
+                console.log('Error compressing image', err);
                 res.sendStatus(500);
-              } else {
-                res.end();
-              }
+              });
+            }).catch(function (err) {
+              console.log('Error resizing image', err);
+              res.sendStatus(500);
             });
-          });
-
-          imagemin.buffer(req.file.buffer, {
-            plugins: [
-              imageminJpegtran(),
-              imageminJpegoptim({ max: 80 }),
-              imageminPngquant({ quality: '65-80' })
-            ]
-          }).then(function(compressedImageBuffer) {
-            blobStream.end(compressedImageBuffer);
-          });
         }
       });
   });
