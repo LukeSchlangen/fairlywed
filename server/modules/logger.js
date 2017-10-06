d22c680c3278357e36e2be5d56432046c9187043
Luke Schlangen <lukeschlangen@gmail.com>
2017-10-05 20:19:24 -0500

    Removing logging to reduce database size

--- server/modules/logger.js
+++ server/modules/logger.js
@@ -1,15 +1,15 @@
 var pool = require('../modules/pg-pool');
 
 function write(userSQLId, action) {
-    pool.connect(function(err, client, done){
-    // Check the user's level of permision based on their email
-    client.query('INSERT INTO logs (user_id, action) VALUES ($1, $2);', [userSQLId, action], function(err, clearanceLevelQueryResult){
-      done();
-      if(err){
-        console.log('Error creating log', err);
-      }
-    });
-  });
+  //   pool.connect(function(err, client, done){
+  //   // Check the user's level of permision based on their email
+  //   client.query('INSERT INTO logs (user_id, action) VALUES ($1, $2);', [userSQLId, action], function(err, clearanceLevelQueryResult){
+  //     done();
+  //     if(err){
+  //       console.log('Error creating log', err);
+  //     }
+  //   });
+  // });
 }
 
 module.exports = { write: write };
